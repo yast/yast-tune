e47bc9686d5bb74deb65ef22f3c7c848abef7f8a
Josef Reidinger <jreidinger@suse.cz>
2014-01-22 09:59:39 +0100

    improved text as suggested by Ken

--- src/clients/hwinfo_proposal.rb
+++ src/clients/hwinfo_proposal.rb
@@ -26,7 +26,7 @@ module Yast
         InitHWinfo.MakeProposal(force_reset)
 
         @ret = {
-          "preformatted_proposal" => "<ul><li><a href=\"hwinfo_proposal\">Show and Edit Hardware Details</a></li></ul>",
+          "preformatted_proposal" => "<ul><li><a href=\"hwinfo_proposal\">System and Hardware Settings</a></li></ul>",
           "links" => ["hwinfo_proposal"]
         }
       # start configuration workflow
