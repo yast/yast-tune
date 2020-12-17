c0c06e1f64b302517daa4e8d077f18f4c867f007
Steffen Winterfeldt <wfeldt@opensuse.org>
2020-12-17 17:04:01 +0100

    remove obsolete unit tests

--- test/system_settings_test.rb
+++ test/system_settings_test.rb
@@ -9,7 +9,6 @@ describe "Yast::SystemSettings" do
   KERNEL_SYSRQ_FILE = "/proc/sys/kernel/sysrq"
 
   subject(:settings) { Yast::SystemSettings }
-  let(:scheduler)     { "cfq" }
   let(:rd_zdev)       { "no-auto" }
   let(:sysctl_config) { CFA::SysctlConfig.new }
 
@@ -17,19 +16,11 @@ describe "Yast::SystemSettings" do
     allow(File).to receive(:exist?).and_return(true)
     allow(Yast::Bootloader).to receive(:Read)
     allow(Yast::Bootloader).to receive(:kernel_param)
-      .with(:common, "elevator").and_return(scheduler)
-    allow(Yast::Bootloader).to receive(:kernel_param)
       .with(:common, "rd.zdev").and_return(rd_zdev)
     allow(CFA::SysctlConfig).to receive(:new).and_return(sysctl_config)
     allow(sysctl_config).to receive(:load)
     allow(sysctl_config).to receive(:save)
     settings.main
-  end
-
-  describe "#GetPossibleElevatorValues" do
-    it "returns an array with possible schedulers" do
-      expect(settings.GetPossibleElevatorValues).to match_array(["cfq", "noop", "deadline"])
-    end
   end
 
   describe "#Read" do
@@ -41,8 +32,6 @@ describe "Yast::SystemSettings" do
       allow(sysctl_config).to receive(:kernel_sysrq).and_return(sysctl_sysrq)
       allow(File).to receive(:read).with(KERNEL_SYSRQ_FILE)
         .and_return(kernel_sysrq)
-      allow(Yast::Bootloader).to receive(:kernel_param)
-        .with(:common, "elevator").and_return(scheduler)
       allow(Yast::Bootloader).to receive(:kernel_param)
         .with(:common, "rd.zdev").and_return(rd_zdev)
       allow(Yast::Mode).to receive(:mode).and_return(mode)
@@ -93,95 +82,16 @@ describe "Yast::SystemSettings" do
         expect(settings.GetAutoConf).to eq(true)
       end
     end
-
-    context "when is in normal mode" do
-      before do
-        allow(Yast::Bootloader).to receive(:Read).and_return(bootloader_read)
-      end
-
-      context "and the bootloader can be read" do
-        let(:bootloader_read) { true }
-
-        it "returns true" do
-          expect(settings.Read).to eq(true)
-        end
-
-        context "when scheduler parameter is missing" do
-          let(:scheduler) { :missing }
-
-          it "unsets IO scheduler" do
-            settings.Read
-            expect(settings.GetIOScheduler).to eq("")
-          end
-        end
-
-        context "when scheduler parameter is present but does not have a value" do
-          let(:scheduler) { :present }
-
-          it "unsets IO scheduler" do
-            settings.Read
-            expect(settings.GetIOScheduler).to eq("")
-          end
-        end
-
-        context "when scheduler parameter has a valid value" do
-          it "sets IO scheduler to that value" do
-            settings.Read
-            expect(settings.GetIOScheduler).to eq(scheduler)
-          end
-        end
-
-        context "when scheduler parameter has an invalid value" do
-          let(:scheduler) { "some-scheduler" }
-
-          it "unsets IO scheduler" do
-            settings.Read
-            expect(settings.GetIOScheduler).to eq("")
-          end
-        end
-
-        it "reads bootloader configuration" do
-          expect(Yast::Bootloader).to receive(:Read)
-          settings.Read
-        end
-      end
-
-      context "but the bootloader cannot be read" do
-        let(:bootloader_read) { false }
-
-        it "returns false" do
-          expect(settings.Read).to eq(false)
-        end
-
-        it "does not set the scheduler" do
-          expect(subject).to_not receive(:SetIOScheduler)
-        end
-      end
-    end
-
-    context "is not in normal mode" do
-      let(:mode) { "installation" }
-
-      it "does not read bootloader configuration" do
-        expect(Yast::Bootloader).to_not receive(:Read)
-        settings.Read
-      end
-    end
   end
 
   describe "#Activate" do
     let(:sysrq_keys) { false }
-    let(:scheduler)  { "" }
-    let(:disk)       { "/sys/block/sda/queue/scheduler" }
-    let(:disk2)      { "/sys/block/sdb/queue/scheduler" }
 
     before do
       settings.SetSysRqKeysEnabled(sysrq_keys)
-      settings.SetIOScheduler(scheduler)
       allow(File).to receive(:write).with(KERNEL_SYSRQ_FILE, anything)
       allow(Yast::Bootloader).to receive(:modify_kernel_params)
       allow(Yast::Bootloader).to receive(:proposed_cfg_changed=)
-      allow(Dir).to receive(:[]).with(/scheduler/).and_return([disk, disk2])
       allow(Dir).to receive(:[]).with("/usr/share/YaST2/locale/*").and_return([])
     end
 
@@ -223,59 +133,6 @@ describe "Yast::SystemSettings" do
         expect(Yast::Bootloader).to receive(:modify_kernel_params)
           .with("rd.zdev" => "no-auto")
         settings.SetAutoConf(false)
-        settings.Activate
-      end
-    end
-
-    context "when a scheduler is set" do
-      let(:scheduler) { "cfq" }
-
-      before do
-        allow(File).to receive(:write).with(KERNEL_SYSRQ_FILE, anything)
-        allow(File).to receive(:read).with(disk).and_return("noop deadline [cfq]")
-        allow(File).to receive(:read).with(disk2).and_return("noop deadline [cfq]")
-      end
-
-      it "updates bootloader configuration" do
-        expect(Yast::Bootloader).to receive(:modify_kernel_params)
-          .with("elevator" => scheduler)
-        expect(Yast::Bootloader).to receive(:proposed_cfg_changed=).with(true)
-        allow(File).to receive(:write)
-        settings.Activate
-      end
-
-      it "activates scheduler for all disk devices" do
-        expect(File).to receive(:write).with(disk, scheduler)
-        expect(File).to receive(:write).with(disk2, scheduler)
-        settings.Activate
-      end
-
-      it "does not activate the scheduler if the device does not support it" do
-        # make the "cfq" scheduler unsupported
-        expect(File).to receive(:read).with(disk2).and_return("[mq-deadline] none")
-        expect(File).to receive(:write).with(disk, scheduler)
-        # ensure it is not changed
-        expect(File).to_not receive(:write).with(disk2, scheduler)
-        settings.Activate
-      end
-    end
-
-    context "when no scheduler is set" do
-      let(:scheduler) { "" }
-
-      before do
-        allow(File).to receive(:write).with(KERNEL_SYSRQ_FILE, anything)
-      end
-
-      it "removes parameter from bootloader configuration" do
-        expect(Yast::Bootloader).to receive(:modify_kernel_params)
-          .with("elevator" => :missing)
-        expect(Yast::Bootloader).to receive(:proposed_cfg_changed=).with(true)
-        settings.Activate
-      end
-
-      it "does not activate scheduler" do
-        expect(File).to_not receive(:write).with(disk, anything)
         settings.Activate
       end
     end
@@ -332,39 +189,6 @@ describe "Yast::SystemSettings" do
       it "does not update sysctl configuration" do
         expect(sysctl_config).to_not receive(:kernel_sysrq=)
         settings.Write
-      end
-    end
-  end
-
-  describe "#SetIOScheduler" do
-    context "when scheduler is a known one" do
-      it "sets the scheduler to the given value" do
-        settings.SetIOScheduler("cfq")
-        expect(settings.GetIOScheduler).to eq("cfq")
-      end
-    end
-
-    context "when scheduler is unknown" do
-      before do
-        settings.SetIOScheduler("cfq")
-      end
-
-      it "does not modify the scheduler" do
-        settings.SetIOScheduler("some-scheduler")
-        expect(settings.GetIOScheduler).to eq("cfq")
-      end
-    end
-
-    context "when new scheduler is different from previous one" do
-      before do
-        allow(Yast::Bootloader).to receive(:kernel_param)
-          .with(:common, "elevator").and_return("cfq")
-        settings.Read
-      end
-
-      it "sets the module as modified" do
-        expect { settings.SetIOScheduler("noop") }.to change { settings.Modified }
-          .from(false).to(true)
       end
     end
   end
